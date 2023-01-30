59621a46417e87f0fc10e47cfd96e2a5eee102f9
Favour Max-Oti <maxotif@gmail.com>
2023-01-30 23:26:10 +0100

    setup apps

---
+++ prisma/seed.js
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+
+import { links } from "../data/links.js";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      email: "maxotif@gmail.com",
+      role: "ADMIN",
+    },
+  });
+
+  await prisma.link.createMany({
+    data: links,
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
